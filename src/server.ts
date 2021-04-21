f5fdd4cea2da180e99632c41243c19f4f560e4e9
levibarbieri-bit <levibarieri@gmail.com>
2021-04-20 23:16:58 -0300

    structured database, created routes, instated 'typeorm', created repository and migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
 import express from "express";
-
+import "./database"
+import {routes} from "./routes";
 const app = express();
-
-app.get("/", (request, response ) => {
-    return response.send("Olá Mundo");
-});
-
-app.post("/users", (request, response) => {
-    return response.json({message: "Salvo com Sucesso!!!"});
-});
-
-app.listen(3333)
+app.use(express.json());
+app.use(routes);
+app.listen(6000)
